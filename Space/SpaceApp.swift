133c3be2ebb92e128739582c400b76a487f64278
latinstar <ian.grimaldi@gmail.com>
2023-04-27 19:08:25 -0400

    Initial Commit

---
+++ Space/SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceApp.swift
+//  Space
+//
+//  Created by Ian on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
